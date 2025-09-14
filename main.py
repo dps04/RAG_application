6c43bc076e121c4bd82d3c01b494643296f8bdc5
dps04 <149152900+dps04@users.noreply.github.com>
2025-09-14 15:59:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,46 @@
-# from pdf_reader_fun import pdf_reader
-# from rag import run
-# import os
-# from dotenv import load_dotenv
-# os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-# os.environ["LANGCHAIN_TRACING_V2"] = "true"
-# print("Let's start ")
-# vector_db = pdf_reader(r"C:\Users\dhirender rana\Downloads\BerkshireHillsBancorpInc_20120809_10-Q_EX-10.16_7708169_EX-10.16_Endorsement Agreement.pdf")
-# print("your data has been procesed")
-# query = input("enter your query")
-# ans = run(vector_db,query)
-# print(ans)
-#  you are a Legal expert. Who are the parties involved in the agreement
-from deepeval.metrics import (
-    ContextualPrecisionMetric,
-    ContextualRecallMetric,
-    ContextualRelevancyMetric
-)
+from retrival import run_llm
+import streamlit as st
+from streamlit_chat import message
+st.header("LangChain Udemy Course")
 
-contextual_precision = ContextualPrecisionMetric()
-contextual_recall = ContextualRecallMetric()
-contextual_relevancy = ContextualRelevancyMetric()
-from deepeval.test_case import LLMTestCase
+prompt = st.text_input("Prompt", placeholder="Enter your prompt..")
 
-test_case = LLMTestCase(
-    input="I'm on an F-1 visa, gow long can I stay in the US after graduation?",
-    actual_output="You can stay up to 30 days after completing your degree.",
-    expected_output="You can stay up to 60 days after completing your degree.",
-    retrieval_context=[
-        """If you are in the U.S. on an F-1 visa, you are allowed to stay for 60 days after completing
-        your degree, unless you have applied for and been approved to participate in OPT."""
-    ]
-)
+if "user_prompt_history" not in st.session_state:
+    st.session_state["user_prompt_history"] = []
 
-contextual_precision.measure(test_case)
-print("Score: ", contextual_precision.score)
-print("Reason: ", contextual_precision.reason)
+if "chat_answers_history" not in st.session_state:
+    st.session_state["chat_answers_history"] = []
 
-contextual_recall.measure(test_case)
-print("Score: ", contextual_recall.score)
-print("Reason: ", contextual_recall.reason)
 
-contextual_relevancy.measure(test_case)
-print("Score: ", contextual_relevancy.score)
-print("Reason: ", contextual_relevancy.reason)
+def create_sources_string(source_urls: set[str]) -> str:
+    if not source_urls:
+        return ""
+    sources_list = list(source_urls)
+    sources_list.sort()
+    sources_string = "sources:\n"
+    for i, source in enumerate(sources_list):
+        sources_string += f"{i+1}. {source}\n"
+    return sources_string
 
-from deepeval import evaluate
 
-evaluate(
-    test_cases=[test_case],
-    metrics=[contextual_precision, contextual_recall, contextual_relevancy]
-)
+if prompt:
+    with st.spinner("Generating response.."):
+        generated_response = run_llm(query=prompt)
+        sources = set(
+            [doc.metadata["source"] for doc in generated_response["source_documents"]]
+        )
+
+        formatted_response = (
+            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+        )
+
+        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["chat_answers_history"].append(formatted_response)
+
+if st.session_state["chat_answers_history"]:
+    for generated_response, user_query in zip(
+        st.session_state["chat_answers_history"],
+        st.session_state["user_prompt_history"],
+    ):
+        message(user_query, is_user=True)
+        message(generated_response)
